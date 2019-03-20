f3af9d59851fc34952ac9cb5fb9f981b0bb03d9c
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-03-20 13:35:04 +0545

    initial commit

---
+++ ApplicationSolution/API/Application.Api/Controllers/IdentityController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        public IConfiguration Configuration { get; } 
+        public IdentityController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        } 
+
+
+        [AllowAnonymous]
+        [Route("getTokenAccess")]
+        public async Task<IActionResult> GetToken()
+        {
+            // discover endpoints from metadata
+            var identityServerEndpoint = Configuration["IdentityServerEndPoint:Default"];
+            var disco = await DiscoveryClient.GetAsync(identityServerEndpoint);
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+             
+            } 
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                
+            }
+
+            return new JsonResult(tokenResponse.Json);
+        }
+
+        [AllowAnonymous]
+        [Route("getTokenPassword")]
+        public async Task<IActionResult> GetTokenPassword()
+        {
+            // discover endpoints from metadata
+            var identityServerEndpoint = Configuration["IdentityServerEndPoint:Default"];
+            var disco = await DiscoveryClient.GetAsync(identityServerEndpoint);
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+
+            }
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password", "api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error); 
+            } 
+
+            return new JsonResult(tokenResponse.Json);
+        }
+
+    }
+}
---
+++ IdeasHubsSolution/src/IdentityServer/IdeasHubs.Server.IdentityServer/Config.cs
@@ -0,0 +1,126 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Test;
+using IdentityServer4;
+
+namespace Application.IdentityServer.Server
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    //AllowOfflineAccess = true,
+                    //AccessTokenLifetime = 60,
+                    //IdentityTokenLifetime = 60,
+                    //AuthorizationCodeLifetime = 60,
+                    //SlidingRefreshTokenLifetime = 60,
+                    //AlwaysIncludeUserClaimsInIdToken = true,
+                    //AccessTokenType = AccessTokenType.Jwt,
+
+
+                    //AbsoluteRefreshTokenLifetime = 60*30*24,
+                    //RefreshTokenExpiration =  TokenExpiration.Absolute,
+                    //RefreshTokenUsage = TokenUsage.ReUse,
+                    //UpdateAccessTokenClaimsOnRefresh = true,
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+               // resource owner password grant client
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+
+                // OpenID Connect implicit flow client (MVC)
+               new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+                    RequireConsent = false,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris           = { "http://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                         "api1"
+                    },
+                    AllowOfflineAccess = true,
+                    AllowAccessTokensViaBrowser = true  
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+    }
+}
